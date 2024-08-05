ca474163b5289a80c4eeb4de413771cc286c10ca
Joe di Stefano <joeydi@okaypl.us>
2024-08-04 21:55:35 -0400

    remove scrolltrigger

--- src/routes/Guidelines.tsx
+++ src/routes/Guidelines.tsx
@@ -1,17 +1,10 @@
-import { useEffect, useRef, RefObject, MutableRefObject } from 'react';
-import { gsap } from 'gsap';
-import { ScrollTrigger } from 'gsap/ScrollTrigger';
 import { getRandom } from '../utils';
 import { fluid, MQ, SM, XXL } from '../styles';
 import Section from '../components/Section';
 import File from '../components/File';
-import { css } from '@emotion/react';
-
-gsap.registerPlugin(ScrollTrigger);
 
 const pages = [
   '/guidelines/Intro.jpg',
-  '/guidelines/Intro-1.jpg',
   '/guidelines/Logo.jpg',
   '/guidelines/Logo-1.jpg',
   '/guidelines/Logo-2.jpg',
@@ -24,6 +17,7 @@ const pages = [
   '/guidelines/Logo-9.jpg',
   '/guidelines/Color.jpg',
   '/guidelines/Color-1.jpg',
+  '/guidelines/Intro-1.jpg',
   '/guidelines/Color-2.jpg',
   '/guidelines/Color-3.jpg',
   '/guidelines/Typography.jpg',
@@ -44,36 +38,6 @@ const pages = [
 ];
 
 export default function Guidelines() {
-  const pinRef = useRef(null);
-  const carouselRef: RefObject<HTMLDivElement> = useRef(null);
-  const scrollTrigger: MutableRefObject<gsap.core.Tween | null> = useRef(null);
-
-  useEffect(() => {
-    if (!scrollTrigger.current) {
-      scrollTrigger.current = gsap.to(carouselRef.current, {
-        x: () => {
-          const carouselWidth = carouselRef?.current?.clientWidth;
-          const scrollWidth = carouselRef?.current?.scrollWidth;
-          if (carouselWidth && scrollWidth) {
-            return scrollWidth * -1 + carouselWidth;
-          }
-          return 0;
-        },
-        scrollTrigger: {
-          trigger: pinRef.current,
-          pin: true,
-          start: 'center center',
-          end: () => {
-            const scrollWidth = carouselRef?.current?.scrollWidth || 1;
-            return `+=${scrollWidth * 0.5}`;
-          },
-          scrub: 1,
-        },
-        ease: 'none',
-      });
-    }
-  }, []);
-
   return (
     <Section>
       <h2>Brand Guidelines</h2>
@@ -82,16 +46,16 @@ export default function Guidelines() {
         visual consistency, while the condensed letterforms and slighter heavier weight improve readability at small sizes.
       </p>
       <File href="/QCDG Brand Guidelines.pdf" target="_blank" name="QCDG Brand Guidelines.pdf" size="12.5 MB" />
-      <div ref={pinRef} css={{ marginTop: fluid(36, 96, SM, XXL) }}>
+      <div css={{ marginTop: fluid(36, 96, SM, XXL) }}>
         <div
-          ref={carouselRef}
           css={{
             '--marginX': fluid(8, 24, SM, XXL),
-            display: 'flex',
-            alignItems: 'center',
-            gap: '1rem',
             marginLeft: 'calc(var(--marginX) * -1)',
             marginRight: 'calc(var(--marginX) * -1)',
+            display: 'flex',
+            flexDirection: 'column',
+            alignItems: 'center',
+            gap: '2rem',
 
             [MQ.xxl]: {
               marginLeft: 'calc((100vw - var(--container-max-width)) / -3)',
@@ -105,14 +69,18 @@ export default function Guidelines() {
                 src={page}
                 alt=""
                 css={{
-                  flexShrink: 0,
-                  aspectRatio: 1920 / 1080,
                   width: '100%',
+                  aspectRatio: 1920 / 1080,
                   objectFit: 'cover',
                   transform: `rotate(${getRandom(-1, 1)}deg)`,
                   border: '4px solid white',
                   borderRadius: '8px',
                   boxShadow: '0 1px 3px rgba(0,0,0,0.125)',
+
+                  '@media (min-aspect-ratio: 1920 / 1080)': {
+                    width: 'auto',
+                    height: '90vh',
+                  },
                 }}
               />
             );
